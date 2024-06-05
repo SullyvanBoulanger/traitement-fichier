383367b7e851166635526c91d5700e2e0eabdc50
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-06-05 12:08:23 +0200

    refacto: Remove unused imports

--- src/main/java/fr/traitement_fichier/entities/BaseEntity.java
+++ src/main/java/fr/traitement_fichier/entities/BaseEntity.java
@@ -4,12 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
-// @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
